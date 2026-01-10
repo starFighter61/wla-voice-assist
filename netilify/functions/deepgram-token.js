c2027401ff545de1b3dffb0e53d41d6693f3fba5
starFighter61 <dzivetz@gmail.com>
2026-01-10 00:22:50 -0800

    Add site + Netlify function

--- netilify/functions/deepgram-token.js
+++ netilify/functions/deepgram-token.js
@@ -1,7 +1,10 @@
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ ok: true, msg: "Function deployed!" }),
-  };
+    // This tells Netlify: "Go get the key from the secure dashboard"
+    const key = process.env.DEEPGRAM_API_KEY;
+
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ access_token: key }),
+    };
 };
